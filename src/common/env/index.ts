d3fa69cd3e6f280cc82d7e7e311517d4692c1efb
iseyoshitaka <ise0615@gmail.com>
2021-11-26 22:32:20 +0900

    CircleCIでビルドが通るようにした

--- src/common/env/index.ts
+++ src/common/env/index.ts
@@ -4,19 +4,18 @@ import { IEnv } from '@/@types/IEnv'
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
 console.log('process.env.BUILD_ENV: ', process.env.BUILD_ENV)
 
-// let Env: IEnv;
-// if (process.env.BUILD_ENV === 'docker') {
-//   /** docker のビルド環境の環境変数から値を取ってくる */
-//   Env = {
-//     envName: process.env.envName,
-//     internalEndpointUrl: process.env.internalEndpointUrl,
-//     externalEndpointUrl: process.env.externalEndpointUrl
-//   } as IEnv
-// } else {
-/** docker でビルドされていない場合は、 .env.local から値を取ってくる */
-// Env = EnvLocal;
-// }
-const Env: IEnv = EnvLocal
+let Env: IEnv
+if (process.env.BUILD_ENV === 'product') {
+  /** docker のビルド環境の環境変数から値を取ってくる */
+  Env = {
+    envName: 'product',
+    internalEndpointUrl: process.env.internalEndpointUrl,
+    externalEndpointUrl: process.env.externalEndpointUrl,
+  } as IEnv
+} else {
+  /** docker でビルドされていない場合は、 .env.local から値を取ってくる */
+  Env = EnvLocal
+}
 
 console.log('Env:', Env)
 
