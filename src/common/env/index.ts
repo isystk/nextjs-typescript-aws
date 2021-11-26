b24636c68b0225ee964d3282af0a4d013d8c7957
iseyoshitaka <ise0615@gmail.com>
2021-11-26 23:04:54 +0900

    Buildが通らないので修正

--- src/common/env/index.ts
+++ src/common/env/index.ts
@@ -5,7 +5,7 @@ console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
 console.log('process.env.BUILD_ENV: ', process.env.BUILD_ENV)
 
 let Env: IEnv
-if (process.env.BUILD_ENV === 'product') {
+if (process.env.BUILD_ENV === 'production') {
   /** docker のビルド環境の環境変数から値を取ってくる */
   Env = {
     envName: 'product',
