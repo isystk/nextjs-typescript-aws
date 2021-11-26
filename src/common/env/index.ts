2b32d0f9b828354911eaa75c0e15d113d8199063
iseyoshitaka <ise0615@gmail.com>
2021-11-27 00:06:53 +0900

    環境変数が本番で利用できないので仕方なく固定

--- src/common/env/index.ts
+++ src/common/env/index.ts
@@ -8,8 +8,9 @@ if (process.env.NODE_ENV === 'production') {
   /** docker のビルド環境の環境変数から値を取ってくる */
   Env = {
     envName: 'product',
-    internalEndpointUrl: process.env.internalEndpointUrl,
-    externalEndpointUrl: process.env.externalEndpointUrl,
+    internalEndpointUrl: 'https://nextjs-typescript-aws.vercel.app',
+    externalEndpointUrl:
+      'https://obew4p54y9.execute-api.ap-northeast-1.amazonaws.com/Prod',
   } as IEnv
 } else {
   /** docker でビルドされていない場合は、 .env.local から値を取ってくる */
